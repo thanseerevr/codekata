1d080da9827f000101f0aaf7beebe1e9db659178
mohamed thanseer <47732366+thanseerevr@users.noreply.github.com>
2019-02-19 15:24:09 +0530

    Update program3.py

--- program3.py
+++ program3.py
@@ -1,8 +1,8 @@
 p=raw_input()
 if(p.isalpha()):
     if(p=='a'or p=='e' or p=='0' or p=='u'):
-        print('Vowel')
-              else
+        print('Vowel')        
+else:
      print('Consonant')
-              else
-              print('Invalid')
+else:
+     print('Invalid')
