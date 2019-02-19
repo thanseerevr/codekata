b6800e3d10b95b7bae9ace3d9e1cf2bdbe167ad0
mohamed thanseer <47732366+thanseerevr@users.noreply.github.com>
2019-02-19 15:20:19 +0530

    Update program3.py

--- program3.py
+++ program3.py
@@ -1,14 +1,8 @@
-#include <stdio.h>
-int main()
-{
-    char d;
-    printf("Enter a character: ");
-    scanf("%d",&d);
-
-    if( (d>='a' && d<='z') || (d>='A' && d<='Z'))
-        printf("%d is an alphabet.",d);
-    else
-        printf("%d is not an alphabet.",d);
-
-    return 0;
-}
+p=raw_input()
+if(p.isalpha()):
+    if(c=='a'or c=='e' or c=='0' orc=='u'):
+        print('vowel')
+              else
+     print('consonant')
+              else
+              print('invalid')
