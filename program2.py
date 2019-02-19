3c6387c1d9d107187bd62dbac32800fe9a6c62b8
mohamed thanseer <47732366+thanseerevr@users.noreply.github.com>
2019-02-19 15:14:08 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,16 +1,5 @@
-#include <stdio.h>
-int main()
-{{
-    int number;
-
-    printf("Enter an integer: ");
-    scanf("%d", &number);
-
-    // True if the number is perfectly divisible by 2
-    if(number % 2 == 0)
-        printf("%d is even.", number);
-    else
-        printf("%d is odd.", number);
-
-    return 0;
-}}
+c=int(raw_input())
+if(0==(c%2)):
+    print("Even")
+else:
+    print("Odd")
