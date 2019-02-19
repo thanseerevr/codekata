38b214bf8170960e2a279fde1d75f06b4d69ca49
mohamed thanseer <47732366+thanseerevr@users.noreply.github.com>
2019-02-19 11:42:40 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -7,12 +7,12 @@ int main()
     scanf("%lf", &number);
 
     if (number <= 0.0)
-    {
+    {{
         if (number == 0.0)
             printf("You entered 0.");
         else
             printf("You entered a negative number.");
-    }
+    }}
     else
         printf("You entered a positive number.");
     return 0;
