7066d4c24c62a6dfb52100c4ef6e2ce1ee3a3197
mohamed thanseer <47732366+thanseerevr@users.noreply.github.com>
2019-02-19 14:07:22 +0530

    Create program9.py

---
+++ program9.py
@@ -0,0 +1,17 @@
+i = 6
+         - - 2 
+         - 1 2 
+temp[] has two elements, 1 and 2 with
+counts as 1 and 2 respectively.
+
+i = 7
+           - 2 
+         3 1 2 
+temp[] has three elements, 3, 1 and 2 with
+counts as 1, 1 and 2 respectively.
+
+i = 8          
+         3 - 2
+         3 1 2 
+temp[] has three elements, 3, 1 and 2 with
+counts as 2, 1 and 2 respectively.
