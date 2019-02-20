ca35cdf117e6b0ed98124f83ac7d6a4809c504ab
mohamed thanseer <47732366+thanseerevr@users.noreply.github.com>
2019-02-20 10:07:56 +0530

    Update program4.py

--- program4.py
+++ program4.py
@@ -1,14 +1,5 @@
-#include <stdio.h>
-int main()
-{{
-    char d;
-    printf("Enter a character: ");
-    scanf("%d",&d);
-
-    if( (d>='a' && d<='z') || (d>='A' && d<='Z'))
-        printf("%d is an alphabet.",d);
-    else
-        printf("%d is not an alphabet.",d);
-
-    return 0;
-}}
+ch=input("character:")
+if((ch>='a' and ch<='z') or (ch>='A" and ch<='Z')):
+        print(ch,"is an alphabet")
+else:
+        print(ch,  "not an alphabet")
