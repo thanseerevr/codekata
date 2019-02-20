20af49bdd836e55537ea11386a9b60c1ce30ee66
mohamed thanseer <47732366+thanseerevr@users.noreply.github.com>
2019-02-20 10:25:40 +0530

    Update program6.py

--- program6.py
+++ program6.py
@@ -1,24 +1,5 @@
-#include <stdio.h>
-
-int yr;
-  printf ("Enter a year \n");
-  scanf ("%d", &yr);
-
-  if (yr%4 == 0) {
-
-      if(yr%100 == 0) {
-      
-          if(yr%400 == 0)
-             printf("\n It is LEAP YEAR.");
-          else
-             printf("\n It is NOT LEAP YEAR.");
-      }
-
-      else {{
-             printf ("\n It is LEAP YEAR.");
-      }}
-  }
-  else
-      printf("\n It is NOT LEAP YEAR.");
-  
-return 0;
+a=int(raw_input())
+if(a%4==0 and a%100!=0 or a%400==0):
+  print("yes")
+else:
+  print("no")
