816f66a45fcdc7629ce1725e08c56cb4b6762304
mohamed thanseer <47732366+thanseerevr@users.noreply.github.com>
2019-02-20 10:10:57 +0530

    Update program4.py

--- program4.py
+++ program4.py
@@ -1,5 +1,5 @@
 ch=input("character:")
-if((ch>='a' and ch<='z') or (ch>='A" and ch<='Z')):
-        print(ch,"is an alphabet")
+if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
+        print(ch,"is an Alphabet")
 else:
-        print(ch,  "not an alphabet")
+        print(ch,  "not an Alphabet")
