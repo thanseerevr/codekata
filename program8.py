d48cd5fa7ffcf9b31dabe79b1df1f9be9ce2f924
mohamed thanseer <47732366+thanseerevr@users.noreply.github.com>
2019-02-20 10:36:01 +0530

    Update program8.py

--- program8.py
+++ program8.py
@@ -1,19 +1,5 @@
-#include <stdio.h>
-int main()
-{{
-    int n, i, sum = 0;
-    
-    printf("Enter a positive integer: ");
-    scanf("%c",&n);
-
-    i = 1;
-    while ( i <=n )
-    {
-        sum += i;
-        ++i;
-    }
-
-    printf("Sum = %c",sum);
-
-    return 0;
-}}
+z=int(raw_input())
+x=z+1
+cyz*x
+d=y/2
+print(d)
